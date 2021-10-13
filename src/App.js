cf30045e7fa7a61cbf097d166f878eece10371e2
Kristina_Vannan <kristina.vannan@ssense.com>
2021-10-13 16:28:11 -0400

    basic weather data displaying

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Weather from "./components/Weather";
+import Input from "../src/components/Input";
+import WeatherData from "../src/components/WeatherData";
+import { API_KEY } from "./apis/config";
 
 function App() {
+  const [weather, setWeather] = useState([]);
+
+  async function fetchData(e) {
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    e.preventDefault();
+    const apiData = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => data);
+    if (city && country) {
+      setWeather({
+        data: apiData,
+        city: apiData.city,
+        country: apiData.sys.country,
+        description: apiData.weather[0].description,
+        temperature: (apiData.main.temp - 273.15).toFixed(2),
+        error: "",
+      });
+    } else {
+      setWeather({
+        data: "",
+        city: "",
+        country: "",
+        description: "",
+        temperature: "",
+        error: "Please Type A City And Country",
+      });
+    }
+  }
+
   return (
     <div className="App">
-      <Weather
-        dt={1602104400 * 1000}
-        temp_min="22.67"
-        temp_max="24.39"
-        main="Clear"
-        icon="01d"
+      <h3>WEATHER APP</h3>
+      <Input getWeather={fetchData} />
+      <WeatherData
+        city={weather.city}
+        country={weather.country}
+        description={weather.description}
+        temperature={weather.temperature}
+        error={weather.error}
       />
+      {console.log(weather.data)}
     </div>
   );
 }
