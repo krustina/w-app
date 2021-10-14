64f40007379227b1a41edf35f0db9308b139f775
Kristina_Vannan <kristina.vannan@ssense.com>
2021-10-14 13:48:45 -0400

    added weather gifs

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,13 +1,25 @@
 import React from "react";
+import "../App.css";
 
-const Weather = ({ weatherData }) => (
-  <div>
-    <div>
-      <h1>{weatherData.name}</h1>
+const Weather = ({ weatherData }) => {
+  //   const desc = weatherData.weather[0].main;
+  const desc = "Smoke";
+  const imgUrl = `weather/${desc}.gif`;
+  return (
+    <div
+      className="background-gif"
+      style={{
+        height: `100vh`,
+        backgroundImage: `url(${imgUrl})`,
+        backgroundSize: `cover`,
+        overflow: `none`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>{weatherData.name}</h1>
       <p>{weatherData.main.temp}° C</p>
       <p>{weatherData.weather[0].main}</p>
     </div>
-  </div>
-);
+  );
+};
 
 export default Weather;
